89a57a1b1d6610ad10e5e30b393067a4eba60f14
gipuk <cubek@o2.pl>
2020-02-23 19:49:45 +0100

    final

--- src/test/java/Tests/Calc1.java
+++ src/test/java/Tests/Calc1.java
@@ -1,36 +1,49 @@
 package Tests;
 
 import org.junit.Test;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import pageobjects.CalcPage;
 import pageobjects.MainPage;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
 
 
 public class Calc1 extends BaseTest {
 
     @Test
-    public void Calctest () {
+    public void CalcTest () {
+        //Go to web2.0calc page
         MainPage mainPage = new MainPage(driver);
         CalcPage calcPage = new CalcPage(driver);
         mainPage.goToWebCalc();
-
         mainPage.acceptCookies();
 
+
+        //Calculate  35*999+(100/4)
         calcPage.multiplyNumbers("35","999");
         calcPage.plusClick();
         calcPage.addbracketLeft();
         calcPage.divideNumbers("100", "4");
+        calcPage.addbracketRight();
         calcPage.clickEqualBtn();
-        calcPage.getResult("34990");
+        calcPage.openHistoryDropdown();
+        calcPage.getResult("first","34990");
 
+        //Calculate cos(pi)
+        calcPage.clickClearBtn();
+        calcPage.clickRadBtn();
+        calcPage.clickCosBtn();
+        calcPage.clickPiBtn();
+        calcPage.clickEqualBtn();
+        calcPage.getResult("next one", "-1");
+
+        //Calculate sqrt(81)
+        calcPage.clickClearBtn();
+        calcPage.sqrtNumber("81");
+        calcPage.clickEqualBtn();
+        calcPage.getResult("next one", "9");
+
+        //History dropdown assertion
+        calcPage.getHistoryResults("9", "-1", "34990");
 
     }
 }
